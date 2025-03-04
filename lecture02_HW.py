2c8c94910990014f729dc22ae480624cbd63f3ea
София Бабина <babinasonya20@gmail.com>
2025-03-04 11:48:39 +0700

    Homework lecture02 Babina

---
+++ lecture02_HW.py
@@ -0,0 +1,232 @@
+def number_to_words(num):
+    # Проверяем диапазон чисел
+    if num < 0 or num > 99:
+        print("Вы ввели неправильное число.")
+        return
+
+    # Обрабатываем числа от 0 до 19
+    if num == 0:
+        print("ноль")
+    elif num == 1:
+        print("один")
+    elif num == 2:
+        print("два")
+    elif num == 3:
+        print("три")
+    elif num == 4:
+        print("четыре")
+    elif num == 5:
+        print("пять")
+    elif num == 6:
+        print("шесть")
+    elif num == 7:
+        print("семь")
+    elif num == 8:
+        print("восемь")
+    elif num == 9:
+        print("девять")
+    elif num == 10:
+        print("десять")
+    elif num == 11:
+        print("одиннадцать")
+    elif num == 12:
+        print("двенадцать")
+    elif num == 13:
+        print("тринадцать")
+    elif num == 14:
+        print("четырнадцать")
+    elif num == 15:
+        print("пятнадцать")
+    elif num == 16:
+        print("шестнадцать")
+    elif num == 17:
+        print("семнадцать")
+    elif num == 18:
+        print("восемнадцать")
+    elif num == 19:
+        print("девятнадцать")
+
+    # Обрабатываем числа от 20 до 29
+    elif num == 20:
+        print("двадцать")
+    elif num == 21:
+        print("двадцать один")
+    elif num == 22:
+        print("двадцать два")
+    elif num == 23:
+        print("двадцать три")
+    elif num == 24:
+        print("двадцать четыре")
+    elif num == 25:
+        print("двадцать пять")
+    elif num == 26:
+        print("двадцать шесть")
+    elif num == 27:
+        print("двадцать семь")
+    elif num == 28:
+        print("двадцать восемь")
+    elif num == 29:
+        print("двадцать девять")
+
+    # Обрабатываем числа от 30 до 39
+    elif num == 30:
+        print("тридцать")
+    elif num == 31:
+        print("тридцать один")
+    elif num == 32:
+        print("тридцать два")
+    elif num == 33:
+        print("тридцать три")
+    elif num == 34:
+        print("тридцать четыре")
+    elif num == 35:
+        print("тридцать пять")
+    elif num == 36:
+        print("тридцать шесть")
+    elif num == 37:
+        print("тридцать семь")
+    elif num == 38:
+        print("тридцать восемь")
+    elif num == 39:
+        print("тридцать девять")
+
+    # Обрабатываем числа от 40 до 49
+    elif num == 40:
+        print("сорок")
+    elif num == 41:
+        print("сорок один")
+    elif num == 42:
+        print("сорок два")
+    elif num == 43:
+        print("сорок три")
+    elif num == 44:
+        print("сорок четыре")
+    elif num == 45:
+        print("сорок пять")
+    elif num == 46:
+        print("сорок шесть")
+    elif num == 47:
+        print("сорок семь")
+    elif num == 48:
+        print("сорок восемь")
+    elif num == 49:
+        print("сорок девять")
+
+    # Обрабатываем числа от 50 до 59
+    elif num == 50:
+        print("пятьдесят")
+    elif num == 51:
+        print("пятьдесят один")
+    elif num == 52:
+        print("пятьдесят два")
+    elif num == 53:
+        print("пятьдесят три")
+    elif num == 54:
+        print("пятьдесят четыре")
+    elif num == 55:
+        print("пятьдесят пять")
+    elif num == 56:
+        print("пятьдесят шесть")
+    elif num == 57:
+        print("пятьдесят семь")
+    elif num == 58:
+        print("пятьдесят восемь")
+    elif num == 59:
+        print("пятьдесят девять")
+
+    # Обрабатываем числа от 60 до 69
+    elif num == 60:
+        print("шестьдесят")
+    elif num == 61:
+        print("шестьдесят один")
+    elif num == 62:
+        print("шестьдесят два")
+    elif num == 63:
+        print("шестьдесят три")
+    elif num == 64:
+        print("шестьдесят четыре")
+    elif num == 65:
+        print("шестьдесят пять")
+    elif num == 66:
+        print("шестьдесят шесть")
+    elif num == 67:
+        print("шестьдесят семь")
+    elif num == 68:
+        print("шестьдесят восемь")
+    elif num == 69:
+        print("шестьдесят девять")
+
+    # Обрабатываем числа от 70 до 79
+    elif num == 70:
+        print("семьдесят")
+    elif num == 71:
+        print("семьдесят один")
+    elif num == 72:
+        print("семьдесят два")
+    elif num == 73:
+        print("семьдесят три")
+    elif num == 74:
+        print("семьдесят четыре")
+    elif num == 75:
+        print("семьдесят пять")
+    elif num == 76:
+        print("семьдесят шесть")
+    elif num == 77:
+        print("семьдесят семь")
+    elif num == 78:
+        print("семьдесят восемь")
+    elif num == 79:
+        print("семьдесят девять")
+
+    # Обрабатываем числа от 80 до 89
+    elif num == 80:
+        print("восемьдесят")
+    elif num == 81:
+        print("восемьдесят один")
+    elif num == 82:
+        print("восемьдесят два")
+    elif num == 83:
+        print("восемьдесят три")
+    elif num == 84:
+        print("восемьдесят четыре")
+    elif num == 85:
+        print("восемьдесят пять")
+    elif num == 86:
+        print("восемьдесят шесть")
+    elif num == 87:
+        print("восемьдесят семь")
+    elif num == 88:
+        print("восемьдесят восемь")
+    elif num == 89:
+        print("восемьдесят девять")
+
+    # Обрабатываем числа от 90 до 99
+    elif num == 90:
+        print("девяносто")
+    elif num == 91:
+        print("девяносто один")
+    elif num == 92:
+        print("девяносто два")
+    elif num == 93:
+        print("девяносто три")
+    elif num == 94:
+        print("девяносто четыре")
+    elif num == 95:
+        print("девяносто пять")
+    elif num == 96:
+        print("девяносто шесть")
+    elif num == 97:
+        print("девяносто семь")
+    elif num == 98:
+        print("девяносто восемь")
+    elif num == 99:
+        print("девяносто девять")
+
+# Основной блок программы
+num = input("Введите число от 0 до 99: ")
+
+# Проверяем, является ли введённое значение числом
+if num.isdigit():
+    number_to_words(int(num))
+elif not num.isdigit():
+    print("Вы должны ввести число.")
